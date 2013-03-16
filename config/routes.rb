59e32ca71258d1ab0336ea4b34adbc461801de3c
jaycevdl <jayce@vanstudiophotography.com>
2013-03-16 15:34:52 -0600

    devise installation/troubleshoot

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Omrails::Application.routes.draw do
  
+ devise_for :users
+
   get 'about' => 'pages#about'
+  
+  root :to => 'pages#home' #sets root as pages controller and home action
 
-  root :to => 'pages#home'
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
